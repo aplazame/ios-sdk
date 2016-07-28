ed6b300d635b75f1134408359413a450523338ff
andresbrun <andresbrunmoreno@gmail.com>
2016-07-28 14:39:26 +0200

    Refactoring.

---
+++ Aplazame-ios-sdk-demo/Aplazame-ios-sdk-demo/OrderExtension.swift
@@ -0,0 +1,18 @@
+//
+//  OrderExtension.swift
+//  Aplazame-ios-sdk-demo
+//
+//  Created by Andres Brun Moreno on 10/07/16.
+//  Copyright © 2016 Andres Brun Moreno. All rights reserved.
+//
+
+import AplazameSDK
+import Foundation
+
+extension Order {
+    mutating func addRandomArticles() {
+        addArticle(.create("id1", name: "RELOJ EN ORO BLANCO DE 18 QUILATES Y DIAMANTES", description: "description", url: NSURL(string: "http://www.chanel.com/es_ES/Relojeria/relojes_joyer%C3%ADa#reloj-en-oro-blanco-de-18-quilates-y-diamantes-J10211")!, imageUrl: NSURL(string: "https://i.imgur.com/1nIay4X.jpg")!, quantity: 2, price: 3993))
+        addArticle(.create("id2", name: "N°5 EAU PREMIERE SPRAY", description: "description", url: NSURL(string: "http://www.chanel.com/en_US/fragrance-beauty/Fragrance-N%C2%B05-N%C2%B05-88145/sku/138083")!, imageUrl: NSURL(string: "https://i.imgur.com/CZ5UPbl.jpg")!, price: 3509))
+        addArticle(.create("id2", name: "ILLUSION D'OMBRE", description: "description", url: NSURL(string: "http://www.chanel.com/en_US/fragrance-beauty/Makeup-Eyeshadow-ILLUSION-D%27OMBRE-122567")!, imageUrl: NSURL(string: "https://i.imgur.com/4j2ib6w.jpg")!, price: 1573))
+    }
+}
--- Aplazame-ios-sdk-demo/Aplazame-ios-sdk-demo/ViewController.swift
+++ Aplazame-ios-sdk-demo/Aplazame-ios-sdk-demo/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var accessTokenTextField: UITextField!
     @IBAction func openCheckout(sender: AnyObject) {
-        presentViewController(createAplazameCheckoutVC(), animated: true, completion: nil)
+        if let text = accessTokenTextField.text where !text.isEmpty {
+            presentViewController(createAplazameCheckoutVC(text), animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
@@ -35,8 +37,8 @@ class ViewController: UIViewController {
         return order
     }()
     
-    private var checkout: Checkout {
-        let config = Config(accessToken: accessTokenTextField.text ?? "", environment: .Sandbox)
+    private func createCheckout(token: String) -> Checkout {
+        let config = Config(accessToken: token, environment: .Sandbox)
         var checkout = Checkout.create(self.order, config: config)
         checkout.addRandomShippingInfo()
         checkout.addRandomCustomer()
@@ -46,12 +48,14 @@ class ViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let destination = segue.destinationViewController as? OrderTableViewController {
-            destination.checkout = checkout
+            destination.checkout = createCheckout("")
         }
     }
 
-    private func createAplazameCheckoutVC() -> AplazameCheckoutViewController {
-        return AplazameCheckoutViewController.create(checkout, delegate: self)
+    private func createAplazameCheckoutVC(token: String) -> AplazameCheckoutViewController {
+        let checkoutVC = AplazameCheckoutViewController.create(createCheckout(token), delegate: self)
+        checkoutVC.modalTransitionStyle = .CrossDissolve
+        return checkoutVC
     }
 }
 
@@ -74,25 +78,4 @@ extension ViewController: AplazameCheckoutDelegate {
     }
 }
 
-extension Order {
-    mutating func addRandomArticles() {
-        addArticle(.create("id1", name: "RELOJ EN ORO BLANCO DE 18 QUILATES Y DIAMANTES", description: "description", url: NSURL(string: "http://www.chanel.com/es_ES/Relojeria/relojes_joyer%C3%ADa#reloj-en-oro-blanco-de-18-quilates-y-diamantes-J10211")!, imageUrl: NSURL(string: "https://i.imgur.com/1nIay4X.jpg")!, quantity: 2, price: 3993))
-        addArticle(.create("id2", name: "N°5 EAU PREMIERE SPRAY", description: "description", url: NSURL(string: "http://www.chanel.com/en_US/fragrance-beauty/Fragrance-N%C2%B05-N%C2%B05-88145/sku/138083")!, imageUrl: NSURL(string: "https://i.imgur.com/CZ5UPbl.jpg")!, price: 3509))
-        addArticle(.create("id2", name: "ILLUSION D'OMBRE", description: "description", url: NSURL(string: "http://www.chanel.com/en_US/fragrance-beauty/Makeup-Eyeshadow-ILLUSION-D%27OMBRE-122567")!, imageUrl: NSURL(string: "https://i.imgur.com/4j2ib6w.jpg")!, price: 1573))
-    }
-}
 
-extension Checkout {
-    mutating func addRandomShippingInfo() {
-        let address = Address.create("Fernando", lastName: "Cabello", street: "Torre Picasso, Plaza Pablo Ruiz Picasso 1", city: "Madrid", state: "Madrid", locale: .currentLocale(), postcode: "28020")
-        shippingInfo = .create("Fernando", price: 500, address: address)
-    }
-    
-    mutating func addRandomCustomer() {
-        customer = .create("140", email: "dev@aplazame.com", gender: .Male, type: .Existing)
-    }
-    
-    mutating func addRandomBillingInfo() {
-        billingInfo = BillingInfo.create("Frank", lastName: "Costello", street: "Torre Picasso, Plaza Pablo Ruiz Picasso 1", city: "Madrid", state: "Madrid", locale: .currentLocale(), postcode: "28020")
-    }
-}
