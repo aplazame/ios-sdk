9f2c83c99af49c463183eba35e709c6b5ea0f53a
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2017-12-04 19:33:27 +0100

    Add missing data in merchant

--- Source/Models/Merchant.swift
+++ Source/Models/Merchant.swift
@@ -22,9 +22,25 @@ struct Merchant {
      */
     let successUrl: String
     /**
+     url that the customer is sent to after order is pending of confirmation.
+     */
+    let pendingUrl: String
+    /**
+     url that the customer is sent in case the credit has been denied.
+     */
+    let koURL: String
+    /**
      url that the customer is sent to if the customer chooses to back to the eccommerce, by default is /.
      */
     let checkoutUrl: String
+    /**
+     (Sólo aplica si la API de confirmación está configurada) Establece el tiempo máximo, en minutos, del que dispone el usuario para completar el proceso de checkout. (por omisión 2880; mín. 1; máx. 2880 )
+     */
+    let timeoutCheckout: TimeInterval = 2880
+    /**
+     (Sólo aplica si la API de confirmación está configurada) Indica si el resultado de la solicitud de crédito aprobada por Aplazame debe completarse informando al usuario en el proceso de checkout
+     */
+    let confirmOnCheckout: Bool = true
     /**
      config object to define environment information
     */
@@ -33,7 +49,13 @@ struct Merchant {
 
 extension Merchant {
     static func create(_ config: Config) -> Merchant {
-        return Merchant(confirmationUrl: "/confirmation.html", cancelUrl: "/cancel.html", successUrl: "/success.html", checkoutUrl: "/checkout.html", config: config)
+        return Merchant(confirmationUrl: "/confirmation",
+                        cancelUrl: "/cancel",
+                        successUrl: "/success",
+                        pendingUrl: "/pending",
+                        koURL: "/ko",
+                        checkoutUrl: "/checkout",
+                        config: config)
     }
 }
 
@@ -44,6 +66,12 @@ extension Merchant {
         record["cancel_url"] = cancelUrl
         record["success_url"] = successUrl
         record["checkout_url"] = checkoutUrl
+        record["checkout_url"] = checkoutUrl
+        record["pending_url"] = pendingUrl
+        record["ko_url"] = koURL
+        record["timeout_checkout"] = timeoutCheckout
+        record["confirm_on_checkout"] = confirmOnCheckout
+        
         return record >< config.record
     }
 }
